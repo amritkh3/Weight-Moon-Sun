b0989572961488e60160b7f9e0f5c796095c9fd9
Amrit khatri <amritkhatri95@gmail.com>
2022-08-01 13:29:16 -0400

    this is first commit

---
+++ weigthOnMoonAndSun.py
@@ -0,0 +1,17 @@
+"""lets make a calculator to measure weight on moon and sun
+*declare a variable with a name "weight"
+*ask user to input their weight in lb
+*assign input value to the variable
+*change the value to number by using float()function.
+*declare one more variable weightinMoon and assign the value weight/6.1
+*declareo ne more variable weightinSun and assign the value weight*27.1
+*display the value with weightinMoon:,weightinMoon,weightinSun:,weightinSun
+
+
+
+"""
+weight=input("enter your weight ")
+weight=float(weight)
+weightOnMoon=weight/6.1
+weightOnSun=weight*27.1
+print("your weight on moon: ",weightOnMoon,"lbs","your weight on sun: ",weightOnSun,"lbs")
